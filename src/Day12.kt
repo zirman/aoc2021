9d4b6273747575ef3bd2789823a9c6498235448b
Robert Chrzanowski <robert.chrzanowski@gmail.com>
2021-12-12 10:32:57 -0500

    Day 12 optimizations.

--- src/Day12.kt
+++ src/Day12.kt
@@ -1,82 +1,71 @@
+
+
 fun main() {
-    fun search(paths: Map<String, MutableList<String>>): List<List<String>> {
-        fun recur(cave: String, visited: Set<String>): List<List<String>> {
-            if (visited.contains(cave)) {
-                return emptyList()
-            } else if (cave == "end") {
-                return listOf(listOf("end"))
-            }
-
-            val vis = if (cave[0].isUpperCase()) {
-                visited
-            } else {
-                visited.plus(cave)
-            }
-
-            return (paths[cave]
-                ?.flatMap { next -> recur(next, vis) }
-                ?.map { listOf(listOf(cave), it).flatten() }
-                ?: emptyList())
-        }
-
-        return recur("start", emptySet())
-    }
-
     fun part1(input: List<String>): Int {
         val paths = mutableMapOf<String, MutableList<String>>()
 
         input.forEach { line ->
             val cols = line.split("-")
             paths.getOrPut(cols[0]) { mutableListOf() }.add(cols[1])
-            paths.getOrPut(cols[1]) { mutableListOf() }.add(cols[0])
+            // don't link back to starting cave
+            if (cols[0] != "start" && cols[1] != "end") {
+                paths.getOrPut(cols[1]) { mutableListOf() }.add(cols[0])
+            }
         }
 
-        return search(paths).size
+        fun recur(path: List<String>, cave: String): Sequence<List<String>> =
+            when {
+                // check for base cases
+                cave == "end" -> sequenceOf(path.plus("end"))
+                cave[0].isLowerCase() && path.contains(cave) -> emptySequence()
+                else -> paths[cave]
+                    ?.asSequence()
+                    ?.flatMap { nextCave ->
+                        recur(path.plus(cave), nextCave)
+                    }
+                    ?.map { listOf(listOf(cave), it).flatten() }
+                    ?: emptySequence()
+            }
+
+        return recur(emptyList(), "start").count()
     }
 
     fun part2(input: List<String>): Int {
-        val smallCaves = mutableSetOf<String>()
+        val paths = mutableMapOf<String, MutableList<String>>()
 
         input.forEach { line ->
             val cols = line.split("-")
-            if (cols[0][0].isLowerCase() && cols[0] != "start" && cols[0] != "end") {
-                smallCaves.add(cols[0])
-            }
-            if (cols[1][0].isLowerCase() && cols[1] != "start" && cols[1] != "end") {
-                smallCaves.add(cols[1])
+            paths.getOrPut(cols[0]) { mutableListOf() }.add(cols[1])
+            // don't link back to starting cave
+            if (cols[0] != "start" && cols[1] != "end") {
+                paths.getOrPut(cols[1]) { mutableListOf() }.add(cols[0])
             }
         }
 
-        return smallCaves.flatMap { smallCave ->
-            val paths = mutableMapOf<String, MutableList<String>>()
-            val alias = "$smallCave-alias"
-
-            input.forEach { line ->
-                val cols = line.split("-")
-                paths.getOrPut(cols[0]) { mutableListOf() }.add(cols[1])
-                paths.getOrPut(cols[1]) { mutableListOf() }.add(cols[0])
-
-                if (cols[0] == smallCave) {
-                    paths.getOrPut(alias) { mutableListOf() }.add(cols[1])
-                    paths.getOrPut(cols[1]) { mutableListOf() }.add(alias)
-                }
-
-                if (cols[1] == smallCave) {
-                    paths.getOrPut(cols[0]) { mutableListOf() }.add(alias)
-                    paths.getOrPut(alias) { mutableListOf() }.add(cols[0])
-                }
+        fun recur(path: List<String>, cave: String, hasSecond: Boolean): Sequence<List<String>> =
+            when {
+                // check for base cases
+                cave == "end" -> sequenceOf(path.plus("end"))
+                cave[0].isLowerCase() && path.contains(cave) && hasSecond -> emptySequence()
+                else -> paths[cave]
+                    ?.asSequence()
+                    ?.flatMap { nextCave ->
+                        recur(
+                            path.plus(cave),
+                            nextCave,
+                            hasSecond || (cave[0].isLowerCase() && path.contains(cave))
+                        )
+                    }
+                    ?.map { listOf(listOf(cave), it).flatten() }
+                    ?: emptySequence()
             }
 
-            search(paths).map { path ->
-                path.map { if (it == alias) smallCave else it }
-            }
-        }.toSet().size
+        return recur(emptyList(), "start", false).count()
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day12_test")
     check(part1(testInput) == 226)
-    check(part2(testInput) == 3509)
 
     val input = readInput("Day12")
     println(part1(input))
