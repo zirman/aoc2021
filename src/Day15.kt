1e4310fe38052d0211d1d75f968a5c41a8532e92
Robert Chrzanowski <robert.chrzanowski@gmail.com>
2021-12-15 16:58:40 -0500

    Day 15 more optimizations.

--- src/Day15.kt
+++ src/Day15.kt
@@ -1,23 +1,24 @@
 import java.util.PriorityQueue
 import kotlin.math.absoluteValue
 
+data class AStar(val x: Int, val y: Int, val g: Int, val h: Int, val gh: Int = g + h)
+
 fun main() {
     fun search(risks: List<List<Int>>): Int {
         val targetX = risks[0].size - 1
         val targetY = risks.size - 1
 
-        fun h(x: Int, y: Int): Int {
-            return ((x - targetX).absoluteValue + (y - targetY).absoluteValue)
-        }
+        fun h(x: Int, y: Int): Int =
+            ((x - targetX).absoluteValue + (y - targetY).absoluteValue)
 
         val maxRisk: List<MutableList<Int?>> =
             risks.map { row -> row.indices.map { null }.toMutableList() }
 
         val queue =
-            PriorityQueue<Triple<Int, Int, Int>>(compareBy { (x, y, g) -> /*h(x, y) + */g })
+            PriorityQueue<AStar>(compareBy { it.gh })
 
-        queue.add(Triple(0, 1, risks[1][0]))
-        queue.add(Triple(1, 0, risks[0][1]))
+        queue.add(AStar(0, 1, risks[1][0], h(0, 1)))
+        queue.add(AStar(1, 0, risks[0][1], h(1, 0)))
         maxRisk[1][0] = risks[1][0]
         maxRisk[0][1] = risks[0][1]
 
@@ -25,21 +26,21 @@ fun main() {
             if (y >= 0 && y < risks.size && x >= 0 && x < risks[0].size) {
                 if (g + risks[y][x] < (maxRisk[y][x] ?: Int.MAX_VALUE)) {
                     maxRisk[y][x] = g + risks[y][x]
-                    queue.add(Triple(x, y, g + risks[y][x]))
+                    queue.add(AStar(x, y, g + risks[y][x], h(x, y)))
                 }
             }
         }
 
         while (true) {
             val next = queue.remove()
-            val (x, y, risk) = next
-            if (next.first == targetX && next.second == targetY) {
-                return risk
+            val (x, y, g, _) = next
+            if (next.x == targetX && next.y == targetY) {
+                return g
             }
-            addQueue(x, y + 1, risk)
-            addQueue(x + 1, y, risk)
-            addQueue(x - 1, y, risk)
-            addQueue(x, y - 1, risk)
+            addQueue(x, y + 1, g)
+            addQueue(x + 1, y, g)
+            addQueue(x - 1, y, g)
+            addQueue(x, y - 1, g)
         }
     }
 
